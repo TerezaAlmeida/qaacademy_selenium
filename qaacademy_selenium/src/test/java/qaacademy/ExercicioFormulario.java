488bfafa9739207d53c29da7fe2166a8eec23bc8
Lucas Silva <terezaarual@hotmail.com>
2023-05-20 15:45:26 -0300

    selenium

---
+++ qaacademy_selenium/src/test/java/qaacademy/ExercicioFormulario.java
@@ -0,0 +1,9 @@
+package qaacademy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExercicioFormulario {
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://www.qaacademy.com.br/");
+}
--- qaacademy_selenium/src/test/java/qaacademy/PrimeiraClasse.java
+++ qaacademy_selenium/src/test/java/qaacademy/PrimeiraClasse.java
@@ -1,14 +1,24 @@
 package qaacademy;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PrimeiraClasse {
 
+    String cursosOnlineBotao = "//*[@id='comp-k7g9c7sd2label']";
+    String cursosPresenciaisBotao = "comp-k7g9c7sd1label";
+    String contatoBotao = "comp-k7g9c7sd3label";
+    String nomeCampo = "input_comp-k37gdip8";
+
     @Test
-    public void testeAberturaNavegador(){
-        WebDriver drive = new ChromeDriver();
-        drive.get("https://www.qaacademy.com.br/");
+    public void testeAberturaNavegador() throws InterruptedException{
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.qaacademy.com.br/");
+        
+        Thread.sleep(6000);
+        driver.findElement(By.id(contatoBotao)).click();
+        driver.findElement(By.id(nomeCampo)).sendKeys("Teste");
     }
 }
