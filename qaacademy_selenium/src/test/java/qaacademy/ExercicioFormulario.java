317206b1faaf4943c2dc4fe964a3d7406f98e535
Lucas Silva <terezaarual@hotmail.com>
2023-05-20 15:46:10 -0300

    g

--- qaacademy_selenium/src/test/java/qaacademy/ExercicioFormulario.java
+++ qaacademy_selenium/src/test/java/qaacademy/ExercicioFormulario.java
@@ -1,9 +1,13 @@
 package qaacademy;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ExercicioFormulario {
-    WebDriver driver = new ChromeDriver();
-    driver.get("https://www.qaacademy.com.br/");
+    @Test
+    public void abrirNavegador(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.qaacademy.com.br/");
+    }
 }
